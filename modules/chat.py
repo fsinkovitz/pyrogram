03f4421fcb9a46d1745540c79d77511a09d92ffd
fsinkovitz <felipesinkovitz@yahoo.com.br>
2021-07-22 20:11:03 -0300

    retirando api_id

--- modules/chat.py
+++ modules/chat.py
@@ -1,8 +1,8 @@
 from pyrogram import Client
 
-api_id = 6766845
-api_hash = "25db9298b5add501ced445e6328ab7e0"
+api_id = 
+api_hash = ""
 
-with Client("minha_conta", 6766845,"25db9298b5add501ced445e6328ab7e0") as app:
+with Client("minha_conta", api_id,"api_hash") as app:
     app.send_message(
-    -1001515748857, "Bom dia rapaziada")
+    chat_id, "Bom dia rapaziada")
--- modules/send_poll.py
+++ modules/send_poll.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 
-api_id = 6766845
-api_hash = "25db9298b5add501ced445e6328ab7e0"
+api_id = 
+api_hash = ""
 
-with Client("minha_conta", 6766845,"25db9298b5add501ced445e6328ab7e0") as app:
-    app.send_poll(-1001515748857, "Prefere o Sim, o Não ou o Talvez?", ["Sim", "Não", "Talvez"])
+with Client("minha_conta", api_id,"api_hash") as app:
+    app.send_poll(chat_id, "Prefere o Sim, o Não ou o Talvez?", ["Sim", "Não", "Talvez"])
